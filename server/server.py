5ad41f3276f688f0a2cbd788b55088b024c42084
Uday Garg <u.garg14@gmail.com>
2020-04-07 13:03:31 +0530

    updated server.py,base.html,main.py

--- server/server.py
+++ server/server.py
@@ -1,58 +1,92 @@
 from socket import AF_INET, socket, SOCK_STREAM
 from threading import Thread
+from person import Person
 import time
 
-#GLOBAL VARIABLES
+# GLOBAL CONSTANTS
 HOST = 'localhost'
 PORT = 5500
 ADDR = (HOST, PORT)
 MAX_CONNECTIONS = 10
 BUFSIZ = 512
 
+# GLOBAL VARIABLES
+persons = []
+SERVER = socket(AF_INET, SOCK_STREAM)
+SERVER.bind(ADDR)  # setup server
 
-def client_communication(client):
+
+def broadcast(msg, name):
+    """
+    send new messages to all clients
+    :param msg: bytes["utf8"]
+    :param name: str
+    :return:
+    """
+    for person in persons:
+        client = person.client
+        try:
+            client.send(bytes(name, "utf8") + msg)
+        except Exception as e:
+            print("[EXCEPTION]", e)
+
+
+def client_communication(person):
     """
     Thread to handle all messages from client
-    :param client: socket
+    :param person: Person
     :return: None
     """
+    client = person.client
 
+    # first message received is always the persons name
+    name = client.recv(BUFSIZ).decode("utf8")
+    person.set_name(name)
+    msg = bytes(f"{name} has joined the chat", "utf8")
+    broadcast(msg, "")  # broadcast welcome message
 
-    run = True
-    while run:
+    while True:  # wait for any messages from person
         msg = client.recv(BUFSIZ)
-        if msg == bytes("{quit}", "utf8"):
-            client.close()
-        else:
+        try:
 
+            if msg == bytes("{quit}", "utf8"):  # if message is quit, disconnect client
+                client.close()
+                persons.remove(person)
+                broadcast(bytes(f"{name} has left the chat...", "utf8"), "")
+
+                print(f"[DISCONNECTED] {name} disconnected")
+                break
+            else:  # otherwise send message to all other clients
+                broadcast(msg, name + ": ")
+                print(f"{name}:", msg.decode("utf8"))
+        except Exception as e:
+            print("[EXCEPTION]", e)
+            break
 
 
 def wait_for_connection():
     """
     Wait for connection from new clients, start new thread once connected
-    :param SERVER: SOCKET
-    :return: NONE
+    :return: None
     """
-    run = True
-    while run:
+
+    while True:
         try:
-            client, addr = SERVER.accept()
+            client, addr = SERVER.accept()  # wait for any new connections
+            person = Person(addr, client) # create new person for connection
+            persons.append(person)
             print(f"[CONNECTION] {addr} connected to the server at {time.time()}")
-            Thread(target=client_communication, args=(client,)).start()
+            Thread(target=client_communication, args=(person,)).start()
         except Exception as e:
-            print("[FAILURE]", e)
-            run = False
+            print("[EXCEPTION]", e)
+            break
 
     print("SERVER CRASHED")
 
 
-
-SERVER = socket(AF_INET, SOCK_STREAM)
-SERVER.bind(ADDR)
-
 if __name__ == "__main__":
-    SERVER.listen(MAX_CONNECTIONS)  #listen for connections
-    print('[STARTED] Waiting for connection...')
+    SERVER.listen(MAX_CONNECTIONS)  # listen for connections
+    print('[STARTED] Waiting for connections..')
     ACCEPT_THREAD = Thread(target=wait_for_connection)
     ACCEPT_THREAD.start()
     ACCEPT_THREAD.join()
--- website/main.py
+++ website/main.py
@@ -1,2 +1,30 @@
 from flask import Flask, render_template, url_for, redirect, request, session
 
+NAME_KEY = 'name'
+
+app = Flask(__name__)
+app.secret_key = "hellomynamesudayandyouwontguessthis"
+
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+
+@app.route("/logout")
+def logout():
+    session.pop(NAME_KEY, None)
+    return redirect(url_for("login"))
+
+
+@app.route("/")
+@app.route("/home")
+def home():
+    if NAME_KEY not in session:
+        return redirect(url_for("login"))
+    name = session[NAME_KEY]
+    return render_template("base.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
